ee392476dc443910109c008ede4f61ed467b43ff
nunkesser <robin.nunkesser@hshl.de>
2024-04-23 14:14:46 +0200

    Service read

--- Thingy52/Thingy52/Pages/ConnectionViewModel.cs
+++ Thingy52/Thingy52/Pages/ConnectionViewModel.cs
@@ -1,76 +1,77 @@
 using System.Diagnostics;
-using System.Reactive.Concurrency;
-using System.Reactive.Linq;
 using Shiny.BluetoothLE;
+using INavigationService = Thingy52.Services.INavigationService;
 
 namespace Thingy52;
 
 public class ConnectionViewModel
 {
     private readonly IBleManager _bleManager;
-    private readonly Services.INavigationService _navigationService;
+    private readonly INavigationService _navigationService;
     private IDisposable? _scanSub;
 
-    public ConnectionViewModel(IBleManager bleManager, Services.INavigationService navigationService)
+    public ConnectionViewModel(IBleManager bleManager,
+        INavigationService navigationService)
     {
-        this._bleManager = bleManager;
+        _bleManager = bleManager;
         _navigationService = navigationService;
         Scan();
     }
 
+    public bool IsScanning { get; set; }
+
     private async void Scan()
     {
         IsScanning = _bleManager?.IsScanning ?? false;
-        
+
         if (_bleManager == null)
-        {
             // handle            
             return;
-        }
-        if (this.IsScanning)
+        if (IsScanning)
         {
-            this.StopScan();
+            StopScan();
         }
         else
         {
-            this.IsScanning = true;
+            IsScanning = true;
 
             async void OnNextScanResults(IList<ScanResult> results)
             {
                 foreach (var result in results)
                 {
-                   // TODO: Search for service id instead of peripheral name
+                    // TODO: Search for service id instead of peripheral name
                     if (result.Peripheral.Name is not "Thingy") continue;
                     StopScan();
 
                     MainThread.BeginInvokeOnMainThread(NavigateToEnvironment);
-                    continue;
 
                     async void NavigateToEnvironment()
                     {
-                        await _navigationService.NavigateToAsync("EnvironmentPage", new Dictionary<string, object>() { { "Peripheral", result.Peripheral } });
+                        await _navigationService.NavigateToAsync(
+                            "EnvironmentPage",
+                            new Dictionary<string, object>
+                            {
+                                { "Peripheral", result.Peripheral }
+                            });
                     }
                 }
             }
 
-            this._scanSub = _bleManager
+            _scanSub = _bleManager
                 .Scan()
                 .Buffer(TimeSpan.FromSeconds(1))
                 .Where(x => x?.Any() ?? false)
-                .ObserveOn(DefaultScheduler.Instance)
+                //.ObserveOn(DefaultScheduler.Instance)
                 .Subscribe(OnNextScanResults,
                     ex => Debug.WriteLine((string?)ex.ToString())
                 );
         }
-        
-    }
-    
-    private void StopScan()
-    {
-        this._scanSub?.Dispose();
-        this._scanSub = null;
-        this.IsScanning = false;
     }
 
-    public bool IsScanning { get; set; }
+    private void StopScan()
+    {
+        _scanSub?.Dispose();
+        _scanSub = null;
+        IsScanning = false;
+    }
 }
--- Thingy52/Thingy52/Pages/Environment/EnvironmentViewModel.cs
+++ Thingy52/Thingy52/Pages/Environment/EnvironmentViewModel.cs
@@ -1,22 +1,41 @@
 using System.Diagnostics;
-using System.Reactive.Concurrency;
-using System.Reactive.Linq;
 using Shiny.BluetoothLE;
 
 namespace Thingy52;
 
-[QueryProperty(nameof(Peripheral), "Peripheral")]
-public class EnvironmentViewModel
+public class EnvironmentViewModel : IQueryAttributable
 {
-    public IPeripheral Peripheral { get; set; }
-    public byte BatteryLevel { get; private set; }
     private readonly IBleManager _bleManager;
-    
-    
+
+
     public EnvironmentViewModel(IBleManager bleManager)
     {
-        this._bleManager = bleManager;
+        _bleManager = bleManager;
     }
 
-    
+    public int Test { get; set; }
+    public IPeripheral? Peripheral { get; set; }
+    public byte BatteryLevel { get; }
+
+
+    public void ApplyQueryAttributes(IDictionary<string, object> query)
+    {
+        Peripheral = query["Peripheral"] as IPeripheral;
+        ReadServices();
+    }
+
+    private void ReadServices()
+    {
+        Peripheral?
+            .WithConnectIf()
+            .Select(x => x.GetServices())
+            .Switch()
+            .Subscribe(OnServicesResult,
+                ex => Debug.WriteLine((string?)ex.ToString()));
+    }
+
+    private void OnServicesResult(IReadOnlyList<BleServiceInfo> obj)
+    {
+        throw new NotImplementedException();
+    }
 }
